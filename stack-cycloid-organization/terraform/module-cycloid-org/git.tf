1933194bdab8f85f64518e3b5bf289a0534ffdb7
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2025-06-16 21:20:25 +0200

    small fix in cycloid org stack

--- stack-cycloid-organization/terraform/module-cycloid-org/git.tf
+++ stack-cycloid-organization/terraform/module-cycloid-org/git.tf
@@ -1,7 +1,7 @@
 resource "cycloid_credential" "git-ssh" {
   name                   = "git-cycloid"
   description            = "SSH private key allowing access to a git repository used as Catalog and Config repositories for your Cycloid organization."
-  organization_canonical = var.dg_name
+  organization_canonical = lower(var.dg_name)
   path                   = "git-cycloid"
   canonical              = "git-cycloid"
 
@@ -16,7 +16,7 @@ resource "cycloid_catalog_repository" "catalog_repo" {
   branch                 = "stacks"
   url                    = var.cycloid_git_url
   credential_canonical   = cycloid_credential.git-ssh.canonical
-  organization_canonical = var.dg_name
+  organization_canonical = lower(var.dg_name)
 }
 
 resource "cycloid_config_repository" "config_repo" {
@@ -25,5 +25,5 @@ resource "cycloid_config_repository" "config_repo" {
   default                = true
   url                    = var.cycloid_git_url
   credential_canonical   = cycloid_credential.git-ssh.canonical
-  organization_canonical = var.dg_name
+  organization_canonical = lower(var.dg_name)
 }
