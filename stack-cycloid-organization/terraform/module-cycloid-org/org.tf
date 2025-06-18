8777b1130f23b5e33cea6aec5bdd37a75c93feb8
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2025-06-18 14:32:15 +0200

    removing cycloid org stack

--- stack-cycloid-organization/terraform/module-cycloid-org/org.tf
+++
@@ -1,8 +0,0 @@
-resource "cycloid_organization" "org" {
-  name                   = var.dg_name
-  organization_canonical = var.cy_org
-
-  lifecycle {
-    ignore_changes = [organization_canonical]
-  }
-}
--- stack-cycloid-organization/terraform/module-tf-backend/variables.tf
+++
@@ -1,9 +0,0 @@
-# Cycloid
-variable "cy_org" {}
-variable "cy_project" {}
-variable "cy_env" {}
-variable "cy_component" {}
-
-variable "dg_name" {
-  description = "The name of the DG."
-}
