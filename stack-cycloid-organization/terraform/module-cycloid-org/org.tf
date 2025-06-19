402390fc5120a39b30065053d56a81a6ea226322
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2025-06-19 15:32:31 +0200

    minor fix

--- stack-cycloid-organization/terraform/module-cycloid-org/org.tf
+++ stack-cycloid-organization/terraform/module-cycloid-org/org.tf
@@ -6,3 +6,8 @@ resource "cycloid_organization" "org" {
     ignore_changes = [organization_canonical]
   }
 }
+
+resource "cycloid_organization_member" "tf_org_member" {
+  email = "olivier.deturckheim@cycloid.io"
+  role_canonical = "organization-admin"
+}
