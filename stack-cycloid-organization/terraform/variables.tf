80ef1e4ee49c520853ea849ee877de4ffcf50e1c
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2025-06-19 17:38:38 +0200

    minor fix

--- stack-cycloid-organization/terraform/variables.tf
+++ stack-cycloid-organization/terraform/variables.tf
@@ -18,17 +18,9 @@ variable "aws_region" {
   default     = "eu-west-1"
 }
 
-variable "aws_sel" {
-  description = "Shall we provision an AWS account in the new DG."
-}
-
 variable "aws_cred_child" {
   description = "Contains AWS access_key and secret_key"
   sensitive   = true
-}
-
-variable "azure_sel" {
-  description = "Shall we provision an AWS account in the new DG."
 }
 
 variable "azure_cred_child" {
